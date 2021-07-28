c94924cc2c1f10ca794a754a3c17046c4dc2b3a0
Lucas Sierota Silva <lucas.silva@codengage.com>
2021-07-27 22:15:15 -0300

    refactor: set up .env example and format python files

--- server/app.py
+++ server/app.py
@@ -5,13 +5,12 @@ from dotenv import dotenv_values
 
 dotenvConfig = dotenv_values(".env")
 # If there is no key in the .env file, then use an empty string
-API_KEY = dotenvConfig.get("WEATHER_KEY") or ""
+API_KEY = dotenvConfig.get("API_KEY") or ""
 
 cacheConfig = {
     "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
-    "CACHE_DEFAULT_TIMEOUT": 60*5,  # 5 minutes of default timeout for Flask-Caching
+    "CACHE_DEFAULT_TIMEOUT": 60 * 5,  # 5 minutes of default timeout for Flask-Caching
 }
-
 
 app = Flask(__name__)
 
@@ -19,32 +18,38 @@ app.config.from_mapping(cacheConfig)
 cache = Cache(app)
 
 
-@app.route('/city/<city>')
-@cache.cached(timeout=60*5)
+@app.route("/city/<city>")
+@cache.cached(timeout=60 * 5)
 def search_city(city):
     if request.method != "GET":
         return make_response(jsonify(error="Method not allowed"), 405)
 
-    if city != request.view_args['city']:
+    if city != request.view_args["city"]:
         return make_response(jsonify(error="Missing city in request"), 402)
 
     # call API and convert response into Python dictionary
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={API_KEY}'
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={API_KEY}"
     response = requests.get(url).json()
 
     # error like unknown city name, inavalid api key
-    if response.get('cod') != 200:
-        return make_response(jsonify(error="Sorry, We couldn't find the specified city."), response.get('cod'))
+    if response.get("cod") != 200:
+        return make_response(
+            jsonify(error="Sorry, We couldn't find the specified city."),
+            response.get("cod"),
+        )
 
     # get current temperature and convert it into Celsius
-    current_temperature = response.get('main', {}).get('temp')
+    current_temperature = response.get("main", {}).get("temp")
     if current_temperature:
         current_temperature_celsius = round(current_temperature - 273.15, 2)
-        data = {'city': city.title(), 'temperature': current_temperature_celsius}
-        return make_response(jsonify(data), response.get('cod'))
+        data = {"city": city.title(), "temperature": current_temperature_celsius}
+        return make_response(jsonify(data), response.get("cod"))
     else:
-        return make_response(jsonify(error=f"Sorry, We couldn't find any city called {city.tile()}."), response.get('cod'))
+        return make_response(
+            jsonify(error=f"Sorry, We couldn't find any city called {city.tile()}."),
+            response.get("cod"),
+        )
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
