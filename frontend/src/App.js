23d51eb17867a4605114274e99ee027454bc8e7e
Lucas Sierota Silva <lucas.silva@codengage.com>
2021-08-01 17:43:21 -0300

    feat: Add components to App
    The App contains all the created components to request and display all the required data.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,37 @@
-import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import React, { useState } from 'react';
+import { Box, ChakraProvider, Container, Flex, theme } from '@chakra-ui/react';
+import { Header } from './Components/atoms/Header';
+import { Search } from './Components/atoms/Search';
+import { CardList } from './Components/molecules/CardList';
+import { WeatherCard } from './Components/molecules/WeatherCard';
 
 function App() {
+  const [searchInput, setSearchInput] = useState('');
+
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <Container maxW="container.xl">
+        <Box mt={12}>
+          <Header />
+        </Box>
+        <Container
+          overflow="visible"
+          minH="80vh"
+          centerContent
+          justifyContent="center"
+        >
+          <Search setSearchInput={setSearchInput} />
+          <Box>
+            <Flex mt={20} align="center" justify="center">
+              <WeatherCard searchInput={searchInput} />
+            </Flex>
+
+            <Flex mt={8} align="center" justify="center">
+              <CardList searchInput={searchInput} />
+            </Flex>
+          </Box>
+        </Container>
+      </Container>
     </ChakraProvider>
   );
 }
